ab8b137fe7753aff7b31c8a261f3a8cf820ade25
Luis López Puig <120658164+LLPFP@users.noreply.github.com>
2024-11-07 17:30:24 +0000

    Práctica 7 empezada ( array, form y acabando conexión casas )

---
+++ practicas/pac07-sombreroSeleccionador/casas.php
@@ -0,0 +1,35 @@
+<?php
+
+// Array multidimensional amb la informació de cada casa
+$casas_info = [
+    "Gryffindor" => [
+    "background_color" => "#740001",
+    "text_color" => "#FFD700",
+    "welcome_message" => "Coratge, valor i determinació. Benvingut a Gryffindor!",
+    "message_background" => "#D3A625",
+    "image" => ""
+    ],
+    "Hufflepuff" => [
+    "background_color" => "#FFDB00",
+    "text_color" => "#60605B",
+    "welcome_message" => "Lleialtat, paciència i treball dur. Benvingut a Hufflepuff!",
+    "message_background" => "#EEE117",
+    "image" => ""
+    ],
+    "Ravenclaw" => [
+    "background_color" => "#0E1A40",
+    "text_color" => "#946B2D",
+    "welcome_message" => "Intel·ligència, creativitat i saviesa. Benvingut a Ravenclaw!",
+    "message_background" => "#5D5D5D",
+    "image" => ""
+    ],
+    "Slytherin" => [
+    "background_color" => "#1A472A",
+    "text_color" => "#AAAAAA",
+    "welcome_message" => "Ambició, astúcia i lideratge. Benvingut a Slytherin!",
+    "message_background" => "#5D5D5D",
+    "image" => ""
+    ]
+];
+
+?>
