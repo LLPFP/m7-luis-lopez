26a5e8950e3a5ec40c398c05683bf05d306df6ac
Luis López Puig <120658164+LLPFP@users.noreply.github.com>
2024-12-12 16:56:31 +0000

    Act 1 - Ex 3

--- practicas/UF2/act1.php
+++ practicas/UF2/act1.php
@@ -3,8 +3,18 @@
 class Llibre
 {
 
+
+
+
     public string $titol = "Don Quijote";
     public string $autor = "Miguel de Cervantes ";
+
+
+    public function _construct(string $titol, string $autor){
+        $this->titol = $titol;
+        $this->autor = $autor;
+    }
+
     public function desripcion(){
         echo "Este es el libro " . $this->titol . " y su autor es: " . $this->autor;
     }
