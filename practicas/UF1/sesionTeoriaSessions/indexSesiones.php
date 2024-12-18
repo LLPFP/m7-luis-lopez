e77f66278b930ff2a5b5049114256d951d823d7f
Luis López Puig <120658164+LLPFP@users.noreply.github.com>
2024-12-18 15:42:50 +0000

    Uf 1 rama uf 2 eliminada

--- practicas/UF1/sesionTeoriaSessions/indexSesiones.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_name('prueba-session');
-session_start();
-
-$_SESSION['username'] = "Juan";
-$_SESSION['age']=23;
-
-?>
---
+++ practicas/UF2/jocderol.php
@@ -0,0 +1,41 @@
+<?php 
+
+class Campeon{
+    public string $nom;
+    public int $hp;
+    public int $atac;
+    public int $defensa;
+    public string $habilidad;
+
+
+    public function _construct($nom, $hp, $atac, $defensa, $habilidad){
+        $this->nom = $nom;
+        $this->hp = $hp;
+        $this->atac = $atac;
+        $this->defensa = $defensa;
+        $this->habilidad = $defensa;
+    }
+
+}
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Joc de Rol</title>
+</head>
+<body>
+
+    
+</body>
+</html>
+
+  
