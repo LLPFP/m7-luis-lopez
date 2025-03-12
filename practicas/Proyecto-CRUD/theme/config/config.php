cba71fc9afa221fbeaaf93362fa6cf493e0a2076
Luis López Puig <120658164+LLPFP@users.noreply.github.com>
2025-03-12 16:36:22 +0000

    Proyecto Reiniciado y conexión BBDD

---
+++ practicas/Proyecto-CRUD/theme/config/config.php
@@ -0,0 +1,17 @@
+
+
+<?php
+$servername = "mysql-luislpfp.alwaysdata.net";
+$username = "luislpfp";
+$password = "ThElement09";
+$dbname = "luislpfp_bduf3";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
